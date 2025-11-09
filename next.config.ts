6ea27b6d7a9e636194bc2bf321d11b656c73817b
Pipo2410 <37326909+Pipo2410@users.noreply.github.com>
2025-11-09 06:02:46 +0000

    posthog setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+      // {
+      //   source: '/relay-wzk4/static/:path*',
+      //   destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      // },
+      // {
+      //   source: '/relay-wzk4/:path*',
+      //   destination: 'https://eu.i.posthog.com/:path*',
+      // },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+}
 
-export default nextConfig;
+export default nextConfig
