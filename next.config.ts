3b29e68d2dec02f903398d788db4f166bcde05d8
Pipo2410 <Pipo2410@gmail.com>
2025-11-09 06:14:11 +0000

    Reverse proxy for vercel master

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,15 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
+      // PostHog tracking
       {
-        source: '/ingest/:path*',
+        source: '/relay-wzk4/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/relay-wzk4/:path*',
         destination: 'https://eu.i.posthog.com/:path*',
       },
-      // {
-      //   source: '/relay-wzk4/static/:path*',
-      //   destination: 'https://eu-assets.i.posthog.com/static/:path*',
-      // },
-      // {
-      //   source: '/relay-wzk4/:path*',
-      //   destination: 'https://eu.i.posthog.com/:path*',
-      // },
     ]
   },
   // This is required to support PostHog trailing slash API requests
